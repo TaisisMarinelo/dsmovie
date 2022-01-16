94b9e6a8dfa423dcd5a69f7a481e96572c8431a4
TaisisMarinelo <taisismarinelo@gmail.com>
2022-01-16 14:40:34 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/services/MovieService.java
+++ backend/src/main/java/com/devsuperior/dsmovie/services/MovieService.java
@@ -10,8 +10,6 @@ import com.devsuperior.dsmovie.dto.MovieDTO;
 import com.devsuperior.dsmovie.entities.Movie;
 import com.devsuperior.dsmovie.repositories.MovieRepository;
 
-
-
 @Service
 public class MovieService {
 	
